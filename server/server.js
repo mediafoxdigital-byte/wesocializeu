2b3a9653baf1e377e497bf57fbc32079f42d7771
mediafoxdigital-byte <mediafoxdigital@gmail.com>
2026-04-30 14:27:08 +0530

    upload

--- server/server.js
+++ server/server.js
@@ -160,7 +160,14 @@ app.use(helmet({
     },
   },
 }));
-app.use(cors({ origin: false, credentials: true }));
+app.use(cors({
+  origin: [
+    /^http:\/\/localhost:\d+$/,
+    /^https:\/\/.*\.vercel\.app$/,
+    'https://wesocializeu.com'
+  ],
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false, limit: '1mb' }));
